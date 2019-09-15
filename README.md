5bf0d3cdf139ab0c9efa9102eb5d28bab84878a5
George Farcasiu <farcasiu@Georges-MacBook-Pro.local>
2019-09-15 16:47:30 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# NYCMesh IPFS Messaging Service
+
+A messaging service for the NYCMesh network that is built on top of IPFS.
+
+Why IPFS? IPFS is decentralized/distributed protocol that enables the service to run without a central point of failure. This messaging service should be able to work even if our network gets partitioned (e.g. a supernode serving an entire neighborhood goes down).
+
+Why a private IPFS network/swarm? By using a "swarm key" we can create an IPFS sub-network only for nodes on NYCMesh. Since IPFS is still in alpha, there are features that are too slow to use on the global network (e.g. the DHT). Many of these performance issues can be avoided buy running IPFS on a smaller network.
+
+Why bootstrap nodes? In order to join to the network a new client needs to connect to an initial set of peers. This project currently uses "bootstrap" nodes, which are just regular IPFS nodes, that have well known IPs on the mesh. Initially, a client connects to a bootstrap node which then tells it about other IPFS nodes on the network. The addresses of these nodes are distributed with the source code.
+
+## Install
+
+Install `node` and then `npm install`
+
+## How To
+
+You can use the messaging client on the command line by invoking `./chat.js`.
+
+Required Command Line Arguments:
+- `--repo-path` a path to the IPFS repo to use (if empty/non-existant a new IPFS repo will be initialized)
+- `--name` your display name
+
+There is also a provided example config for running a bootstrap node.
+
+Please post questions to the #mesh-services NYCMesh slack channel: https://app.slack.com/client/T02MB96L1/C06H99CGY
+
+## Status
+
+This project is still in very early stages. Our main goal right now is to find out whether or not IPFS is a good fit for building decentralized, reliable, and secure services.
---
+++ chat.js
@@ -0,0 +1,180 @@
+#!/usr/local/bin/node
+
+'use strict'
+
+const argv = require("minimist")(process.argv.slice(1))
+
+const Buffer = require("buffer").Buffer
+const fs = require('fs')
+const readline = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+const IPFS = require("ipfs")
+
+const Libp2p = require('libp2p')
+const MPLEX = require('libp2p-mplex')
+const Protector = require('libp2p-pnet')
+const SECIO = require('libp2p-secio')
+const TCP = require('libp2p-tcp')
+const MulticastDNS = require('libp2p-mdns')
+const Gossipsub = require('libp2p-gossipsub')
+
+
+/* CONSTS */
+
+
+const PEER_DISCOVERY_INTERVAL = 2000
+
+const KNOWN_PEERS = [
+    // TODO : replace this with well known peers on the mesh network!
+    "/ip4/0.0.0.0/tcp/4001/ipfs/QmVKZppLgR4ZtxbQvhDCSXkCJVkyARpLdACD1JKzC1xfHG",
+]
+
+const PROMPT = "> "
+
+const DEFAULT_NAME = "nobody"
+const DEFAULT_PASSWORD = "asdfads3241234dfadsfa1234124"
+const DEFAULT_SWARM_KEY_PATH = "./swarm.key"
+const DEFAULT_SWARM_PORT = "4001"
+const DEFAULT_TOPIC = "adspioasdfoiasd;lfkqwenvoicv8uqewrfuq"
+
+
+/* HELPERS */
+
+
+// Copied and adapted from: https://github.com/libp2p/js-libp2p/blob/b2943014562c56f4a88c36471fc54e4b801899b1/examples/pnet-ipfs/libp2p-bundle.js
+const privateLibp2pBundle = (swarmKeyPath) => {
+    /**
+     * This is the bundle we will use to create our fully customized libp2p bundle.
+     *
+     * @param {libp2pBundle~options} opts The options to use when generating the libp2p node
+     * @returns {Libp2p} Our new libp2p node
+     */
+    const libp2pBundle = (opts) => {
+        // Set convenience variables to clearly showcase some of the useful things that are available
+        const peerInfo = opts.peerInfo
+        const peerBook = opts.peerBook
+
+        // TODO : do we need to specify further pubsub configuration?
+        // TODO : better understand the specified defaults here
+        return new Libp2p({
+            peerInfo,
+            peerBook,
+            modules: {
+                transport: [TCP], // We're only using the TCP transport for this example
+                streamMuxer: [MPLEX], // We're only using mplex muxing
+                // Let's make sure to use identifying crypto in our pnet since the protector doesn't
+                // care about node identity, and only the presence of private keys
+                connEncryption: [SECIO],
+                // TODO : verify that this works outside of LAN networks
+                peerDiscovery: [MulticastDNS],
+                connProtector: new Protector(fs.readFileSync(swarmKeyPath)),
+                pubsub: Gossipsub,
+            },
+            config: {
+                peerDiscovery: {
+                    mdns: {
+                        interval: PEER_DISCOVERY_INTERVAL,
+                        enabled: true
+                    }
+                },
+                pubsub: {
+                    // NOTE that this disables self-delivery i.e. not delivering a pubsub message to the publisher of said message
+                    emitSelf: false,
+                }
+            }
+        })
+    }
+
+    return libp2pBundle
+}
+
+const createConfig = (repo_path, swarm_port = DEFAULT_SWARM_PORT, password = DEFAULT_PASSWORD, swarm_key_path = DEFAULT_SWARM_KEY_PATH) => {
+    return {
+        repo: repo_path,
+        // TODO : do we need to init?
+        init: true,
+        pass: password,
+        libp2p: privateLibp2pBundle(swarm_key_path),
+        config: {
+            Bootstrap: [],
+            Addresses: {
+                Swarm: ["/ip4/127.0.0.1/tcp/" + swarm_port]
+            },
+        },
+    }
+}
+
+
+/* CORE */
+
+
+async function main() {
+    if (argv["repo-path"] === undefined) {
+        console.log("FATAL: must provide --repo-path")
+        process.exit()
+    } else {
+        console.log(`INFO: using IPFS repo ${argv["repo-path"]}`)
+    }
+
+    const name = argv["name"] === undefined ? DEFAULT_NAME : argv["name"]
+    const topic = argv["topic"] === undefined ? DEFAULT_TOPIC : argv["topic"]
+
+    const config = createConfig(argv["repo-path"], argv["swarm-port"], argv["password"], argv["swarm-key-path"])
+    const node = await IPFS.create(config)
+
+    console.log("INF0: Bootstrapping...")
+
+    // TODO : use a better algo here (e.g. happy eyeballs) or at least shuffle `KNOWN_PEERS`
+    for (var multihash of KNOWN_PEERS) {
+        try {
+            await node.swarm.connect(multihash)
+            break
+        } catch (e) {
+            console.log(`WARN: Failed to connect to known peer: ${multihash}`)
+        }
+    }
+
+    if ((await node.swarm.peers()).length == 0) {
+        console.log("FATAL: Failed to connect to any peers, exiting.")
+        process.exit()
+    } else {
+        console.log("INFO: Completed boostrapping")
+    }
+
+    readline.setPrompt("> ")
+
+    const writeToConsole = (line) => {
+        console.log("\r".repeat(PROMPT.length) + line)
+        readline.prompt()
+    }
+
+    var peerCount = null
+    setInterval(async () => {
+        const peers = await node.swarm.peers()
+        if (peers.length != peerCount) {
+            writeToConsole(`INFO: Connected to ${peers.length} peers`)
+            peerCount = peers.length
+        }
+    }, 5*1000)
+
+    console.log(`Subscribing to ${topic}...`)
+    await node.pubsub.subscribe(topic, (raw) => {
+        const data = JSON.parse(raw.data.toString())
+        writeToConsole(`${data.name}> ${data.msg}`)
+    })
+
+    readline.on("line", (line) => {
+        node.pubsub.publish(topic, Buffer.from(JSON.stringify({name: name, msg: line})))
+        readline.prompt()
+    })
+    readline.prompt()
+}
+
+
+main().catch((err) => {
+    console.log(err)
+    process.exit()
+})
